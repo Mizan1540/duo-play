8d7c565a98136f27936c618a6e7f498f0868c532
Mizan1540 <mizanmunna9@gmail.com>
2026-09-23 05:19:24 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+void main()=>runApp(MaterialApp(debugShowCheckedModeBanner:false,home: Scaffold(backgroundColor: Color(0xFF0D0D0D), body: Center(child: Text("Duo Play Ready", style: TextStyle(color: Colors.yellow, fontSize: 24))))));
